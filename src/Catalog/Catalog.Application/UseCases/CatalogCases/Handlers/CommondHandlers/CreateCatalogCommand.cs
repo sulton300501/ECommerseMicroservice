0daee439b4dd8979516e3240aaa70af3dd2fc9d3
sulton <sultonmalik326@gmail.com>
2024-04-29 22:11:12 +0500

    Add project files.

---
+++ src/Catalog/Catalog.Application/UseCases/CatalogCases/Handlers/CommondHandlers/CreateCatalogCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.UseCases.CatalogCases.Handlers.CommondHandlers
+{
+    public class CreateCatalogCommand :IRequest
+    {
+
+    }
+}
